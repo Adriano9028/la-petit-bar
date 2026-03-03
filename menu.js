649712bb0d51373db501ece1582f69f5b4cbc681
Adriano9028 <adrianomanoel9028@gmail.com>
2026-03-03 00:57:39 -0300

    Atualizando novas informações

--- menu.js
+++ menu.js
@@ -11,3 +11,15 @@ document.addEventListener("scroll",  ()=>{
     }
 
 })
+
+function menuShow(){
+let menuMobile = document.querySelector('.menu-mobile');
+if (menuMobile.classList.contains('open')){
+    menuMobile.classList.remove('open');
+    document.querySelector('.icon').src ="imagem/menu_white_36dp.svg";
+}else{
+    menuMobile.classList.add('open');
+     document.querySelector('.icon').src ="imagem/close_white_36dp.svg";
+}
+
+}
